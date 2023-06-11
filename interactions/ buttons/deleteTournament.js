63f55859e87a54f679b93d28bd050c979d0e8675
acasaro <asaro913@gmail.com>
2023-06-11 15:37:43 -0700

    added delete tournament logic

--- interactions/ buttons/deleteTournament.js
+++ interactions/ buttons/deleteTournament.js
@@ -1,6 +1,15 @@
+const { config } = require("../../config");
+
 module.exports = {
   id: "delete_tournament",
   async execute(interaction) {
+    const categoryId = interaction.channel.parentId;
+    const channelsToDelete = await interaction.client.channels.cache.filter(
+      (channel) => channel.id === categoryId || channel.parentId === categoryId
+    );
+    channelsToDelete.forEach((channel) => {
+      channel.delete();
+    });
     await interaction.deferUpdate();
     return;
   },
