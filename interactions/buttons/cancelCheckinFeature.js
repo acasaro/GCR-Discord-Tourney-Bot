2235e7f0ae68313fdfb490877d2b894069634a27
acasaro <asaro913@gmail.com>
2023-08-11 17:57:51 -0700

    fixing cancel check-in button so that it actually removes the entire pool of registered users

--- interactions/buttons/cancelCheckinFeature.js
+++ interactions/buttons/cancelCheckinFeature.js
@@ -3,6 +3,7 @@ const {
   deleteRegisteredTournamentUsers,
   updateTournament,
 } = require('../../common/utility-functions');
+const { AdminEmbed } = require('../../embeds/adminEmbed-alt');
 
 module.exports = {
   id: 'cancel_checkin',
@@ -11,17 +12,18 @@ module.exports = {
       const { channel } = interaction;
       const parentChannelId = channel.parentId;
       const tournament = await getTournamentByCategoryId(parentChannelId);
-      const { checkin_message_id, lobby_channel_id } = tournament;
+      const { checkin_message_id, checkin_channel_id, admin_message_id } =
+        tournament;
 
       // Get the guild (server) where the interaction occurred
       const guild = interaction.guild;
 
       // Get the channel where you want to lookup the message
-      const waitingRoomChannel = guild.channels.cache.get(lobby_channel_id);
+      const checkinChannel = guild.channels.cache.get(checkin_channel_id);
 
-      if (!waitingRoomChannel) {
+      if (!checkinChannel) {
         await interaction.reply(
-          'Tournament waiting room channel not found, (it may have been deleted).',
+          'Tournament checkin channel not found, (it may have been deleted).',
         );
         return;
       }
@@ -29,11 +31,25 @@ module.exports = {
       // Delete all registered players
       await deleteRegisteredTournamentUsers(tournament.id);
       // Fetch the specific checkin message by its ID
-      const checkinMessage = await waitingRoomChannel.messages.fetch(
+      const checkinMessage = await checkinChannel.messages.fetch(
         checkin_message_id,
       );
 
-      await checkinMessage.delete(checkin_message_id);
+      await checkinChannel.delete(checkin_message_id);
+
+      // Edit Admin message with updated values
+      const adminMessage = await interaction.channel.messages.fetch(
+        admin_message_id,
+      );
+
+      const updatedAdminMessage = await AdminEmbed({
+        tournament: {
+          ...tournament.dataValues,
+          checkin_active: false,
+        },
+      });
+
+      await adminMessage.edit(updatedAdminMessage);
 
       await updateTournament(tournament.id, {
         checkin_message_id: null,
@@ -44,6 +60,7 @@ module.exports = {
         content: 'Check-in has been canceled',
         embeds: [],
         components: [],
+        ephemeral: true,
       });
     } catch (error) {
       console.log(error);
