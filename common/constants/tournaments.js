f72e67dff63e5d9bea634d36328c58ca0f12bdc6
Anthony Asaro <asaro913@gmail.com>
2023-11-01 10:12:08 -0700

    Update tournaments.js
    updating unicode emoji icons

--- common/constants/tournaments.js
+++ common/constants/tournaments.js
@@ -5,22 +5,22 @@ const teamSizes = {
 };
 
 const ranks = [
-  { name: 'Bronze', value: 0, emoji: '1124040488492740609' },
-  { name: 'Silver', value: 288, emoji: '1124040537889067158' },
-  { name: 'Gold', value: 475, emoji: '1124040520977612860' },
-  { name: 'Platinum I', value: 654, emoji: '1124040524601495683' },
-  { name: 'Platinum II', value: 713, emoji: '1124041423088517201' },
-  { name: 'Platinum III', value: 774, emoji: '1124056512675987488' },
-  { name: 'Diamond I', value: 835, emoji: '1124040501239230644' },
-  { name: 'Diamond II', value: 915, emoji: '1124041169681260614' },
-  { name: 'Diamond III', value: 995, emoji: '1124041171975553114' },
-  { name: 'Champ I', value: 1075, emoji: '1124040490782838914' },
-  { name: 'Champ II', value: 1195, emoji: '1124040494339608686' },
-  { name: 'Champ III', value: 1315, emoji: '1124056510029381643' },
-  { name: 'Grand Champ I', value: 1435, emoji: '1124040507971092590' },
-  { name: 'Grand Champ II', value: 1574, emoji: '1124040511360077935' },
-  { name: 'Grand Champ III', value: 1715, emoji: '1124040513771814932' },
-  { name: 'SSL', value: 1860, emoji: '1124040540518879292' },
+  { name: 'Bronze', value: 0, emoji: '1125075361550585917' },
+  { name: 'Silver', value: 288, emoji: '821845510453329981' },
+  { name: 'Gold', value: 475, emoji: '1125075414067454002' },
+  { name: 'Platinum I', value: 654, emoji: '1125075439317168161' },
+  { name: 'Platinum II', value: 713, emoji: '1125075442328674375' },
+  { name: 'Platinum III', value: 774, emoji: '821845510293159988' },
+  { name: 'Diamond I', value: 835, emoji: '1125075520028168214' },
+  { name: 'Diamond II', value: 915, emoji: '1125075522754445475' },
+  { name: 'Diamond III', value: 995, emoji: '821845510611533935' },
+  { name: 'Champ I', value: 1075, emoji: '1125075551900667915' },
+  { name: 'Champ II', value: 1195, emoji: '1125075555524546600' },
+  { name: 'Champ III', value: 1315, emoji: '1125075557424574616' },
+  { name: 'Grand Champ I', value: 1435, emoji: '1125075578194755696' },
+  { name: 'Grand Champ II', value: 1574, emoji: '1125075581474705518' },
+  { name: 'Grand Champ III', value: 1715, emoji: '1125075583072731197' },
+  { name: 'SSL', value: 1860, emoji: '1125075610977439764' },
 ];
 
 module.exports = {
